e30a0a43011cb1c311c42c9a7c2288ae8743bed4
Your Name <you@example.com>
2021-12-10 18:28:06 +0900

    レポート

--- ex-1-main.cpp
+++ ex-1-main.cpp
@@ -1,105 +1,48 @@
-// ht18a100.cpp
 #include <iostream>
-#include <string>
+#define CPP2_PRIME_UPPER_LIMIT 1000000 
+using namespace std;
 
-namespace cpp2 {
+bool prime(unsigned int n) {
+	if (n == 2) return 1;
+	if (n <= 1 || n % 2 == 0) return 0;
+	for (int i = 3; i * i <= n; i += 2) {
+		if (n % i == 0)
+			return 0;
+	}
 
-	class mcxi {
-	private:
-		char c[10];
-		char z = '0';
-		int m;
-	public:
-		int to_string();
-		mcxi(std::string a) {
-			int n = a.size();
-			m = 0;
-			for (int i = 0; i < n; i++) {
-				if (a[i] >= '2' && a[i] <= '9') {
-					switch (c[i + 1]) {
-					case 'm':
-						m += (c[i + 1] - 'z') * 1000;
-						break;
-					case 'c':
-						m += (c[i + 1] - 'z') * 100;
-						break;
-					case 'x':
-						m += (c[i + 1] - 'z') * 10;
-						break;
-					case 'i':
-						m += (c[i + 1] - 'z');
-						break;
-					default:
-						break;
-					}
-				}
-				else {
-					switch (c[i]) {
-					case 'm':
-						m += 1000;
-						break;
-					case 'c':
-						m += 100;
-						break;
-					case 'x':
-						m += 10;
-						break;
-					case 'i':
-						m += 1;
-						break;
-					default:
-						break;
-					}
-				}
-			}
+	return 1;
+
+}
+
+int nth_prime(unsigned int a, unsigned int d, unsigned int n) {
+	int x = a;
+	unsigned int cnt = 0;
+	if (a % 2 == 0 && d % 2 == 0)return 0;
+	while (cnt < n) {
+		if (prime(x) == 1) {
+			cnt++;
 		}
-		mcxi operator+(const mcxi& a, const mcxi& b) {
-			return a + b;
-		}
-	};
-	int mcxi::to_string() {
+
+		x += d;
 
 	}
-}
+	return x - d;
+
+};
+
 int main() {
-	cpp2::mcxi a0("xi");
-	cpp2::mcxi b0("x9i");
-	cpp2::mcxi result0 = a0 + b0;
-	std::cout << "3x" << " " << result0.to_string() << std::endl;
-	cpp2::mcxi a1("i");
-	cpp2::mcxi b1("9i");
-	cpp2::mcxi result1 = a1 + b1;
-	std::cout << "x" << " " << result1.to_string() << std::endl;
-	cpp2::mcxi a2("c2x2i");
-	cpp2::mcxi b2("4c8x8i");
-	cpp2::mcxi result2 = a2 + b2;
-	std::cout << "6cx" << " " << result2.to_string() << std::endl;
-	cpp2::mcxi a3("m2ci");
-	cpp2::mcxi b3("4m7c9x8i");
-	cpp2::mcxi result3 = a3 + b3;
-	std::cout << "5m9c9x9i" << " " << result3.to_string() << std::endl;
-	cpp2::mcxi a4("9c9x9i");
-	cpp2::mcxi b4("i");
-	cpp2::mcxi result4 = a4 + b4;
-	std::cout << "m" << " " << result4.to_string() << std::endl;
-	cpp2::mcxi a5("i");
-	cpp2::mcxi b5("9m9c9x8i");
-	cpp2::mcxi result5 = a5 + b5;
-	std::cout << "9m9c9x9i" << " " << result5.to_string() << std::endl;
-	cpp2::mcxi a6("m");
-	cpp2::mcxi b6("i");
-	cpp2::mcxi result6 = a6 + b6;
-	std::cout << "mi" << " " << result6.to_string() << std::endl;
-	cpp2::mcxi a7("i");
-	cpp2::mcxi b7("m");
-	cpp2::mcxi result7 = a7 + b7;
-	std::cout << "mi" << " " << result7.to_string() << std::endl;
-	cpp2::mcxi a8("m9i");
-	cpp2::mcxi b8("i");
-	cpp2::mcxi result8 = a8 + b8;
-	std::cout << "mx" << " " << result8.to_string() << std::endl;
-	cpp2::mcxi a9("9m8c7xi");
-	cpp2::mcxi b9("c2x8i");
-	cpp2::mcxi result9 = a9 + b9;
-	std::cout << "9m9c9x9i" << " " << result9.to_string() << std::endl;
+	cout << nth_prime(367, 186, 151) << endl;
+	cout << nth_prime(179, 10, 203) << endl;
+	cout << nth_prime(271, 37, 39) << endl;
+	cout << nth_prime(103, 230, 1) << endl;
+	cout << nth_prime(27, 104, 185) << endl;
+	cout << nth_prime(253, 50, 85) << endl;
+	cout << nth_prime(1, 1, 1) << endl;
+	cout << nth_prime(9075, 337, 210) << endl;
+	cout << nth_prime(307, 24, 79) << endl;
+	cout << nth_prime(331, 221, 177) << endl;
+	cout << nth_prime(259, 170, 40) << endl;
+	cout << nth_prime(269, 58, 102) << endl;
+	cout << nth_prime(4, 4, 5) << endl;
+	return 0;
 }
